f88909e3a29f4de5021006768224f4ff18f32181
E Stringham <backfrip@users.noreply.github.com>
2015-02-13 13:16:17 -0500

    NQueens template started

---
+++ 03infinite/NQueens.java
@@ -0,0 +1,24 @@
+public class NQueens {
+
+    // constuctor
+    public NQueens(int size) {
+    }
+
+    // toString
+    public String toString() {
+    }
+
+    // return student name
+    public String name() {
+    }
+
+
+
+    // solve the board starting from 0,0
+    public boolean solve() {
+    }
+
+    // solve the board starting from x in a rank/file
+    public boolean solve(int x) {
+    }
+}
