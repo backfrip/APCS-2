58eb9cc4eea9eb7119e144b2a404669fcd26ea55
E Stringham <backfrip@users.noreply.github.com>
2015-03-10 03:52:13 -0400

    Dupes as broke as the ones in Garry's Mod

--- 06pikachu/Sorts.java
+++ 06pikachu/Sorts.java
@@ -2,45 +2,50 @@ import java.util.*;
 
 public class Sorts {
     private static void partition(int[] ary, int start, int end) {
-	if (ary.length < 2)
+	if (end - start < 1)
 	    return;
-	
-	System.out.println(Arrays.toString(ary));
-	
+
+	// printFrag("", ary, start, end, " w/ pivot ");
+
 	int h;
-	int pivot = ary[start + (int)(Math.random() * (end - start + 1))];
-	
-	System.out.println("Pivot: " + ary[pivot]);
-	
+	int pivot = start + (int) (Math.random() * (end - start + 1));
+
+	// System.out.print(ary[pivot]);
+
 	h = ary[pivot];
 	ary[pivot] = ary[end];
 	ary[end] = h;
-	
-	System.out.println(Arrays.toString(ary));
-	
-	
+
 	int si = start;
 	int ei = end - 1;
-	int c = 0;
-	while (si != ei) {
-	    if (ary[c] <= ary[end]) {
-		//System.out.println("Moving " + ary[c] + " to the left, swapping with " + ary[si]);
-		h = ary[c];
-		ary[c] = ary[si];
-		ary[si] = h;
+	while (si < ei) {
+	    if (ary[si] <= ary[end]) {
 		si++;
-	    } else if (ary[c] > ary[end]) {
-		System.out.println("Moving " + ary[c] + " to the right, swapping with " + ary[ei]);
-		h = ary[c];
-		ary[c] = ary[ei];
+	    } else {
+		h = ary[si];
+		ary[si] = ary[ei];
 		ary[ei] = h;
 		ei--;
 	    }
-	    c++;
 	}
-	
-	//partition(ary, start, pivot - 1);
-	//partition(ary, pivot + 1, end);
+
+	if (ary[si] > ary[end]) {
+	    pivot = si;
+	    h = ary[pivot];
+	    ary[pivot] = ary[end];
+	    ary[end] = h;
+	} else {
+	    pivot = si + 1;
+	    h = ary[pivot];
+	    ary[pivot] = ary[end];
+	    ary[end] = h;
+	}
+
+	// printFrag(" -> ", ary, start, end, "\n");
+
+	partition(ary, start, pivot - 1);
+	partition(ary, pivot + 1, end);
+
 
     }
 
@@ -52,5 +57,25 @@ public class Sorts {
     public static void main(String[] args) {
 	int[] a = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
 	quicksort(a);
+	System.out.println(Arrays.toString(a));
+
+	int[] b = { 4, 8, 1, 5, 2, 3, 7, 0, 9, 6 };
+	quicksort(b);
+	System.out.println(Arrays.toString(b));
+
+	int[] c = new int[100000];
+	for (int i = 0; i < c.length; i++) {
+	    c[i] = (int)(Math.random() * 10);
+	}
+	quicksort(c);
+    }
+
+    private static void printFrag(String pre, int[] ary, int start, int end,
+	    String post) {
+	System.out.print(pre + "[");
+	for (int i = start; i < end; i++) {
+	    System.out.print(ary[i] + ", ");
+	}
+	System.out.print(ary[end] + "]" + post);
     }
 }
