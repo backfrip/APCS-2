da47165108f4c1708bb11a1db5b7ef17c6ad06d2
E Stringham <backfrip@users.noreply.github.com>
2015-03-13 13:12:01 -0400

    I have an unconvential implementation of the head, and it has now broken my somewhat elegant toString() :(

--- 07robot/Driver.java
+++ 07robot/Driver.java
@@ -1,9 +1,18 @@
 public class Driver {
     public static void main(String[]args) {
 	MyLinkedList fender = new MyLinkedList();
+	System.out.println(fender + ", " + fender.size());
 	fender.add(1);
+	System.out.println(fender + ", " + fender.size());
 	fender.add(2);
-	fender.add(3, 3);
-	System.out.println(fender);
+	System.out.println(fender + ", " + fender.size());
+	fender.add(2, 3);
+	System.out.println(fender + ", " + fender.size());
+	fender.remove();
+	System.out.println(fender + ", " + fender.size());
+	fender.remove();
+	System.out.println(fender + ", " + fender.size());
+	fender.remove();
+	System.out.println(fender + ", " + fender.size());
     }
 }
