ae67c7322682be159c7e7f0010a1fc5be12ab3b6
E Stringham <backfrip@users.noreply.github.com>
2015-03-25 13:12:51 -0400

    Hard to think of a concise way of adding to an index without a bunch of 'if's. Something's wrong with my resize()

---
+++ 10deqedout/Driver.java
@@ -0,0 +1,18 @@
+public class Driver {
+    public static void main(String[]args) {
+	MyDeque deck = new MyDeque();
+	deck.addFirst(1);
+	deck.addFirst(1);
+	deck.addFirst(1);
+	deck.addFirst(1);
+	deck.addFirst(1);
+	deck.addFirst(1);
+	deck.addFirst(1);
+	deck.addFirst(1);
+	deck.addFirst(1);
+	deck.addFirst(1);
+	deck.addFirst(1);
+	deck.addFirst(1);
+	System.out.println(deck);
+    }
+}
--- 10deqedout/MyDeque.java
+++ 10deqedout/MyDeque.java
@@ -1,4 +1,4 @@
-public class MyDeque {
+public class MyDeque<E> {
     private Object[]data;
     private int start, size;
     
@@ -10,5 +10,34 @@ public class MyDeque {
     
     public MyDeque() {
 	this(10);
+    }
+    
+    // temp method
+    public String toString() {
+	String out = "[ ";
+	for (Object val : data) {
+	    out += val + " ";
+	}
+	return out + "]";
+    }
+    
+    public void addFirst(E value) {
+	if (size == data.length)
+	    expand();
+	if (start == 0)
+	    start = data.length - 1;
+	else
+	    start -= 1;
+	data[start] = value;
+    }
+    
+    private void expand() {
+	Object[]temp = new Object[data.length * 2];
+	for (int i = 0; i < size; i++) {
+	    temp[i] = data[(start + i) % size];
+	}
+	data = temp;
+	size *= 2;
+	start = 0;
     }
 }
