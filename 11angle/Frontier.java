c9d745008ac1ee8bacb19820a644c1616e6d2ef9
E Stringham <backfrip@users.noreply.github.com>
2015-04-15 13:12:15 -0400

    Skeleton frontier

--- 11angle/Frontier.java
+++ 11angle/Frontier.java
@@ -1,3 +1,24 @@
-public class Frontier {
+public class Frontier<E> {
+    private Object[] data;
+    private int[] priority;
+    private int start, size;
+    private boolean x;
+
+    public Frontier(int cap) {
+	front = new int[cap][2];
+	start = 0;
+	size = 0;
+    }
+
+    public Frontier() {
+	this(10000);
+    }
     
+    public String toString() {
+	String out = "[ ";
+	for (int i = 0; i < size; i++) {
+	    out += data[(start + i) % data.length] + " ";
+	}
+	return out + "]";
+    }
 }
--- 11angle/Maze.java
+++ 11angle/Maze.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayDeque;
 
 public class Maze {
     // @formatter:off
