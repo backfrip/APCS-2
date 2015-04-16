75c8b0ce53087d6d0d20f980fddf7e8d4bdeb777
E Stringham <backfrip@users.noreply.github.com>
2015-04-15 23:17:33 -0400

    Built Frontier by adapting MyDeque

--- 11angle/Driver.java
+++ 11angle/Driver.java
@@ -2,5 +2,14 @@ public class Driver {
     public static void main(String[]args) {
 	Maze maiz = new Maze("damaze");
 	System.out.println(maiz.toString(true));
+	Frontier<Integer> front = new Frontier<Integer>(true, 1);
+	front.addFirst(10, 6);
+	front.addFirst(12, 5);
+	front.addLast(16, 10);
+	System.out.println(front);
+	front.removeFirst();
+	System.out.println(front);
+	front.removeLast();
+	System.out.println(front);
     }
 }
--- 11angle/Frontier.java
+++ 11angle/Frontier.java
@@ -1,24 +1,121 @@
+import java.util.NoSuchElementException;
+
 public class Frontier<E> {
     private Object[] data;
-    private int[] priority;
+    private int[] prio;
     private int start, size;
-    private boolean x;
+    private boolean priority;
 
-    public Frontier(int cap) {
-	front = new int[cap][2];
+    public Frontier(boolean p, int cap) {
+	if (cap <= 0)
+	    cap = 1;
+	data = new Object[cap];
+	prio = new int[cap];
 	start = 0;
 	size = 0;
+	priority = p;
+    }
+
+    public Frontier(boolean p) {
+	this(p, 10000);
     }
 
     public Frontier() {
-	this(10000);
+	this(false);
     }
-    
+
     public String toString() {
 	String out = "[ ";
-	for (int i = 0; i < size; i++) {
+	for (int i = 0; i < size; i++)
 	    out += data[(start + i) % data.length] + " ";
+	if (priority) {
+	    out += "]\n[ ";
+	    for (int i = 0; i < size; i++)
+		out += prio[(start + i) % data.length] + " ";
 	}
 	return out + "]";
+    }
+
+    public void addFirst(E value) {
+	if (size == data.length)
+	    resize(data.length * 2);
+	start = (data.length + start - 1) % data.length;
+	data[start] = value;
+	size += 1;
+    }
+
+    public void addFirst(E value, int p) {
+	if (!priority)
+	    throw new RuntimeException("This is not a priority queue!");
+	addFirst(value);
+	prio[start] = p;
+    }
+
+    public void addLast(E value) {
+	if (size == data.length)
+	    resize(data.length * 2);
+	data[(start + size) % data.length] = value;
+	size += 1;
+    }
+
+    public void addLast(E value, int p) {
+	if (!priority)
+	    throw new RuntimeException("This is not a priority queue!");
+	addLast(value);
+	prio[(start + size - 1) % data.length] = p;
+    }
+
+    @SuppressWarnings("unchecked")
+    public E removeFirst() throws NoSuchElementException {
+	checkEmpty();
+	if (size == data.length / 4)
+	    resize(data.length / 2);
+	start = (data.length + start + 1) % data.length;
+	size -= 1;
+	return (E) data[(data.length + start - 1) % data.length];
+    }
+
+    @SuppressWarnings("unchecked")
+    public E removeLast() {
+	checkEmpty();
+	if (size == data.length / 4)
+	    resize(data.length / 2);
+	size -= 1;
+	return (E) data[(start + size) % data.length];
+    }
+
+    @SuppressWarnings("unchecked")
+    public E getFirst() {
+	checkEmpty();
+	return (E) data[start];
+    }
+
+    @SuppressWarnings("unchecked")
+    public E getLast() {
+	checkEmpty();
+	return (E) data[(start + size - 1) % data.length];
+    }
+
+
+
+    private void checkEmpty() throws NoSuchElementException {
+	if (size == 0)
+	    throw new NoSuchElementException();
+    }
+
+    private void resize(int cap) {
+	Object[] temp = new Object[cap];
+	for (int i = 0; i < size; i++) {
+	    temp[i] = data[(start + i) % data.length];
+	}
+	data = temp;
+	if (priority) {
+	    int[] tempo = new int[cap];
+	    for (int i = 0; i < size; i++) {
+		tempo[i] = prio[(start + i) % prio.length];
+	    }
+	    prio = tempo;
+	}
+	start = 0;
     }
 }
