a81b9a861c5f2ef1c9de5450c22bc2ce7a15a4d0
E Stringham <backfrip@users.noreply.github.com>
2015-04-23 12:57:45 -0400

    TreeNode<T> is a go

---
+++ 12Vein/BTree.java
@@ -0,0 +1,15 @@
+public class BTree<E> {
+    public static final int PRE_ORDER = 0;
+    public static final int IN_ORDER = 1;
+    public static final int POST_ORDER = 2;
+    
+    private TreeNode<E> root;
+    
+    public BTree() {
+	root = new TreeNode<E>();
+    }
+    
+    public void add(E data) {
+	
+    }
+}
---
+++ 12Vein/TreeNode.java
@@ -0,0 +1,59 @@
+public class TreeNode<T> {
+    private T self
+    TreeNode<T> left, right;
+    private int c;
+    
+    public TreeNode() {
+	this(null, null, null)
+    }
+    
+    public TreeNode(T data) {
+	this(data, null, null);
+    }
+    
+    public TreeNode(T data, TreeNode<T> leftChild) {
+	this(data, leftChild, null);
+    }
+    
+    public TreeNode(T data, TreeNode<T> leftChild, TreeNode<T> rightChild) {
+	self = data;
+	left = leftChild;
+	right = rightChild;
+    }
+    
+    public void setData(T data) {
+	self = data;
+    }
+    
+    public T getData() {
+	return self;
+    }
+    
+    public void setLeft(TreeNode<T> child) {
+	left = child;
+    }
+    
+    public T getLeft() {
+	return left;
+    }
+    
+    public void setRight(TreeNode<T> child) {
+	right = child;
+    }
+    
+    public T getRight() {
+	return right;
+    }
+    
+    public boolean hasData() {
+	return data != null;
+    }
+    
+    public boolean hasLeft() {
+	return left != null;
+    }
+    
+    public boolean hasRight() {
+	return right != null;
+    }
+}
