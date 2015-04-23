41ed7e13d6d7be2a46f49fd1a6471661e5d8b611
E Stringham <backfrip@users.noreply.github.com>
2015-04-23 13:09:41 -0400

    Not a whole lot of progress, briefly misunderstood recursive add()

--- 12Vein/BTree.java
+++ 12Vein/BTree.java
@@ -1,15 +1,27 @@
+import java.util.Random;
+
 public class BTree<E> {
     public static final int PRE_ORDER = 0;
     public static final int IN_ORDER = 1;
     public static final int POST_ORDER = 2;
     
     private TreeNode<E> root;
+    private Random randy;
     
     public BTree() {
+	this(Math.random() * 10000);
+    }
+    
+    public BTree(int seed) {
 	root = new TreeNode<E>();
+	randy = new Random(seed);
     }
     
     public void add(E data) {
 	
+    }
+    
+    public void add(TreeNode<E> parent, TreeNode<E> child) {
+	if ()
     }
 }
